2100e310d579c66fe66139928f2573100831dfec
Serg <bloodfast13@gmail.com>
2020-02-21 00:16:51 +0200

    Updates

---
+++ precache-manifest.819a8795e7a1fcd235c9ecf17b32a8e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61689122c6b5bc06583bae3a00f767d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "04e3e060c22a28bb76fa",
+    "url": "./static/css/main.4dd61811.chunk.css"
+  },
+  {
+    "revision": "ca3c7a53af9b67d2d2eb",
+    "url": "./static/js/2.d50f1fee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.d50f1fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e3e060c22a28bb76fa",
+    "url": "./static/js/main.8f3a24db.chunk.js"
+  },
+  {
+    "revision": "646d0283e68a0af75a6d",
+    "url": "./static/js/runtime-main.92040055.js"
+  },
+  {
+    "revision": "898829f91be18fb2fe61e718e81dee83",
+    "url": "./static/media/google-voice-search-icon-21.898829f9.jpg"
+  },
+  {
+    "revision": "a842ee8012a18f7dca58570d47bfc0f8",
+    "url": "./static/media/google_PNG19625.a842ee80.png"
+  },
+  {
+    "revision": "3b41ef569039e78822a7896236293d5c",
+    "url": "./static/media/menu.3b41ef56.svg"
+  }
+]);
